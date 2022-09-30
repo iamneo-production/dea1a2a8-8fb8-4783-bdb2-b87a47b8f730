89deb54c254acd714fceb87cf51eb4ffed7f463c
Aravinthraj2111 <111455574+Aravinthraj2111@users.noreply.github.com>
2022-09-30 23:51:43 +0530

    Add files via upload

---
+++ Reactapp/src/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
+
+
+import Dashboard from './Dashboard';
+import AdminDashboard from './AdminDashboard';
+import SignUp from './SignUp';
+import FetchApi from './FetchApi';
+import Login1 from './Login1';
+import Course from './Course';
+import CourseAdd from './CourseAdd';
+import CourseDel from './CourseDel';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <div>
+          <div className="header">
+            <NavLink exact activeClassName="active" to="/">Abacus Academy Admission Portal</NavLink>
+            
+            
+          </div>
+          <div className="content">
+            <Switch>
+              <Route exact path="/" component={Login1} />
+              <Route path="/dashboard" component={Dashboard} />
+              <Route path="/signup" component={SignUp} />
+              <Route path="/fetchapi" component={FetchApi}/>       
+              <Route path="/login1" component={Login1}/>
+              <Route path="/admindashboard" component={AdminDashboard}/> 
+              <Route path="/course" component={Course}/>     
+              <Route path="/courseadd" component={CourseAdd}/>
+              <Route path="/coursedelete" component={CourseDel}/>
+            </Switch>
+          </div>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
